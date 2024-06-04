d98a33633f66c1d16e6375e5a9b534efb7dc6952
Baislgan <bpron@yandex.ru>
2024-06-04 15:07:25 +0300

    поменяла информацию в файле

---
+++ tests/test_pokemon.py
@@ -0,0 +1,16 @@
+import requests
+import pytest
+
+URL = 'https://api.pokemonbattle.ru/v2'
+TOKEN = '3271b76b466b4b674886c21d15e636fa'
+HEADER = {'Content-Type':'application/json', 'trainer_token':TOKEN}
+TRAINER_ID = '4064'
+
+def test_status_code():
+    response = requests.get(url=f'{URL}/trainers')
+    assert response.status_code == 200
+
+def test_trainer_name():
+    response = requests.get(url=f'{URL}/trainers', params={'trainer_id' : '4064'})
+    assert response.json()["data"][0]["trainer_name"] == 'Панда'
+
